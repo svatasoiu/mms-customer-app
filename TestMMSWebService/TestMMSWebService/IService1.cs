47723c32fbb4f9db324bed19be60d4fe42eac642
unknown <scvatasoiu@SCVatasoiu-7D.dom1.mms.org>
2014-06-25 11:47:21 -0400

    added test .net wcf web service

---
+++ TestMMSWebService/TestMMSWebService/IService1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TestMMSWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface TestService
+    {
+        [OperationContract]
+        string GetUserName(int ID);
+
+        [OperationContract]
+        List<User> GetAllUsers();
+    }
+
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public int UserID;
+        [DataMember]
+        public string Name;
+        [DataMember]
+        public DateTime ExpirationDate;
+    }
+}
---
+++ TestMMSWebService/TestMMSWebService/Service1.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TestMMSWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : TestService
+    {
+        public string GetUserName(int ID)
+        {
+            return "Sorin";
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return new List<User>() { new User {UserID=1, Name="SV", ExpirationDate=new DateTime(1990,05,05)}};
+        }
+    }
+}
